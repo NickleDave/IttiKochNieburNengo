d32d6a73da5f9496e3c018c2e0456ead1aab871f
NickleDave <nickledave@users.noreply.github.com>
2019-08-29 19:34:58 -0400

    rename vis_ctx to make_vis_ctx, rewrite function

--- src/itti_koch_niebur_nengo/visual_cortex/vis_ctx.py
+++ src/itti_koch_niebur_nengo/visual_cortex/vis_ctx.py
@@ -1,30 +1,32 @@
 import cv2
+import numpy as np
 
-from . import features
 from . import saliency
 
 
-def vis_ctx(img, sigma=8, c_range=(2, 3, 4), delta_range=(3, 4),
-            thetas=(0, 45, 90, 135), ksize=(9, 9), gabor_sigma=4.0,
-            lambd=10, gamma=0.5, psi=0, ktype=cv2.CV_32F,
-            S_sigma=4, minmax=(0, 10), thresh=1, step=16
-            ):
+def make_vis_ctx(img_shape, sigma=8, c_range=(2, 3, 4), delta_range=(3, 4),
+                 thetas=(0, 45, 90, 135), ksize=(9, 9), gabor_sigma=4.0,
+                 lambd=10, gamma=0.5, psi=0, ktype=cv2.CV_32F,
+                 S_sigma=3, minmax=(0, 10), thresh=1, step=16):
     """extract early features from image, create conspicuity channels
     from features maps, and then sum to create S, the input to saliency map"""
     # feature extraction
-    R, G, B, Y, I = features.get_channels(img)
-    I_maps = features.intensity_ftr_maps(I, sigma, c_range, delta_range)
-    RG_maps, BY_maps = features.color_ftr_maps(R, G, B, Y,
-                                               sigma, c_range, delta_range)
-    O_maps = features.orientation_ftr_maps(I, thetas, sigma, c_range,
-                                           delta_range, ksize, gabor_sigma,
-                                           lambd, gamma, psi, ktype)
+    def img_to_S_with_params_fixed():
+        def anon_img_to_S(img):
+            return saliency.img_to_S(img, sigma, c_range, delta_range,
+                                     thetas, ksize, gabor_sigma,
+                                     lambd, gamma, psi, ktype,
+                                     S_sigma, minmax, thresh, step)
+        return anon_img_to_S
 
-    # conspicuity channels, saliency computed from features
-    height, width = I_maps[S_sigma].shape[:2]
-    I_bar = saliency.I_conspicuity(I_maps, width, height, minmax, thresh, step)
-    C_bar = saliency.C_conspicuity(RG_maps, BY_maps, width, height, minmax, thresh, step)
-    O_bar = saliency.O_conspicuity(O_maps, width, height, minmax, thresh, step)
-    S = compute_S(I_bar, C_bar, O_bar, minmax, thresh, step)
+    img_to_S_params_fixd = img_to_S_with_params_fixed()
 
-    return S
+    def vis_ctx():
+        def anon_vis_ctx(x):
+            img = np.reshape(x, img_shape)
+            S = img_to_S_params_fixd(img)
+            return S.flatten()
+        return anon_vis_ctx
+
+    return vis_ctx()
+
