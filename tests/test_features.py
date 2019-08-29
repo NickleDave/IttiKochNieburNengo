a1a98878fe4d7ddefb292074254cde19f8a945ee
NickleDave <nickledave@users.noreply.github.com>
2019-08-29 19:44:05 -0400

    change package name in tests/test_features.py

--- tests/test_features.py
+++ tests/test_features.py
@@ -4,9 +4,6 @@ import unittest
 import cv2
 import numpy as np
 
-import itti_koch_niebur_nengo as iknn
-
-
 HERE = Path(__file__).parent
 TEST_DATA = HERE.joinpath('test_data')
 TEST_IMG_DIR = TEST_DATA.joinpath('img')
@@ -18,7 +15,7 @@ class TestFeatures(unittest.TestCase):
         self.an_image = cv2.imread(self.an_image_path)
 
     def test_get_channels(self):
-        R, G, B, Y, I = iknn.features.get_channels(self.an_image)
+        R, G, B, Y, I = itti_koch_niebur_nengo.visual_cortex.features.get_channels(self.an_image)
         for channel in [R, G, B, Y, I]:
             self.assertTrue(
                 type(channel) == np.ndarray
@@ -28,16 +25,16 @@ class TestFeatures(unittest.TestCase):
             )
 
     def test_gaussian_pyramid(self):
-        R, G, B, Y, I = iknn.features.get_channels(self.an_image)
-        I_pyr = iknn.features.gaussian_pyramid(I)
+        R, G, B, Y, I = itti_koch_niebur_nengo.visual_cortex.features.get_channels(self.an_image)
+        I_pyr = itti_koch_niebur_nengo.visual_cortex.features.gaussian_pyramid(I)
         self.assertTrue(len(I_pyr) == 8)  # default, from paper
         for level in I_pyr:
             self.assertTrue(type(level) is np.ndarray)
 
     def test_center_surround(self):
-        R, G, B, Y, I = iknn.features.get_channels(self.an_image)
-        I_pyr = iknn.features.gaussian_pyramid(I)
-        I_maps = iknn.features.center_surround(I_pyr)
+        R, G, B, Y, I = itti_koch_niebur_nengo.visual_cortex.features.get_channels(self.an_image)
+        I_pyr = itti_koch_niebur_nengo.visual_cortex.features.gaussian_pyramid(I)
+        I_maps = itti_koch_niebur_nengo.visual_cortex.features.center_surround(I_pyr)
         self.assertTrue(len(I_maps) == 6)  # default, from paper
 
 if __name__ == '__main__':
